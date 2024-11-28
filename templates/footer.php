64c91a25259349e409910cc6d64cda9b7bb7674e
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-11-28 00:37:07 -0500

    Create footer.php

---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+    <footer style="margin-top: 20px; padding: 10px; background-color: #f4f4f4; text-align: center;">
+        <p>&copy; <?= date('Y') ?> Uchū Docs All rights reserved.</p>
+    </footer>
+</body>
+</html>
