b7cff8c438bc2ec371c1f06a393334419c871d74
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2025-02-02 17:53:42 -0500

    Update PostRenderPluginInterface.php

--- src/interfaces/PostRenderPluginInterface.php
+++ src/interfaces/PostRenderPluginInterface.php
@@ -5,10 +5,11 @@ namespace Documentation\interfaces;
 /**
  * Interface for plugins that add post-processing to page rendering
  */
-interface PostRenderPluginInterface extends PluginInterface {
+interface PostRenderPluginInterface extends PluginInterface
+{
     /**
      * Perform actions after rendering a page
-     * 
+     *
      * @param string $pageName Name of the page rendered
      * @param string $renderedContent The rendered HTML content
      * @return void
