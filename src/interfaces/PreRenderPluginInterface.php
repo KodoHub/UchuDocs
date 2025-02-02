0f57b8f7c58ab881fd303883634e65b1b857e7fb
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2025-02-02 17:54:02 -0500

    Update PreRenderPluginInterface.php

--- src/interfaces/PreRenderPluginInterface.php
+++ src/interfaces/PreRenderPluginInterface.php
@@ -5,10 +5,11 @@ namespace Documentation\interfaces;
 /**
  * Interface for plugins that add preprocessing to page rendering
  */
-interface PreRenderPluginInterface extends PluginInterface {
+interface PreRenderPluginInterface extends PluginInterface
+{
     /**
      * Perform actions before rendering a page
-     * 
+     *
      * @param string $pageName Name of the page to be rendered
      * @return void
      */
